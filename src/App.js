c060af16297f280fc07887877c95247e152981ba
Dan Siwiec <daniel.siwiec@gmail.com>
2019-10-09 17:38:15 -0700

    Add Lead Form. Fields are not wired up yet

--- src/App.js
+++ src/App.js
@@ -1,49 +1,15 @@
 import React from "react";
-import axios from "axios";
-import Button from '@material-ui/core/Button';
+import LeadForm from './LeadForm';
+import evenClient from './evenClient'
 
 export default class App extends React.Component {
   state = {
     users: []
   };
-  componentDidMount() {
-    this.getRateAndLead();
-  }
-  token = "1fb5853d-2f0a-478f-b83d-94b3c8561349_9732a9ea-2191-4180-a657-c5a2c0d19046"
 
-
-  config = {
-    headers: { 'Authorization': "Bearer " + this.token }
-  };
-
-  bodyParameters = {
-    "productTypes": [
-      "savings"
-    ],
-    "personalInformation": {
-      "state": "NY",
-      "zipcode": "10010"
-    },
-    "creditInformation": {
-      "providedCreditRating": "excellent",
-      "providedNumericCreditScore": 750
-    },
-    "savingsInformation": {
-      "minDepositAmount": 1000
-    },
-    "clientTags": {
-      "hello": [],
-      "something": []
-    }
-  }
-
-  getRateAndLead = () => {
-    axios.post(
-      "https://api.evenfinancial.com/leads/rateTables",
-      this.bodyParameters,
-      this.config
-    ).then((response) => {
-      debugger;
+  submitLeadForm = e =>{
+    e.preventDefault();
+    evenClient.createLead().then((response) => {
       this.setState({
         leadUuid: response.data.leadUuid,
         loanOffers: response.data.loanOffers,
@@ -58,12 +24,9 @@ export default class App extends React.Component {
   render() {
     return (
       <div>
+        <LeadForm submit={this.submitLeadForm}/>
         HELLO Dan Siwiec ( Sheveeitz)
             Here is  you rate - {this.state.leadUuid}
-
-        <Button variant="contained" color="primary">
-          Hello World
-            </Button>
       </div>
     );
   }
