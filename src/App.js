903ebbd3c0f4daae71749e3225df5b5189bf97c6
Dan Siwiec <daniel.siwiec@gmail.com>
2019-10-09 18:08:39 -0700

    Remove unused imports

--- src/App.js
+++ src/App.js
@@ -1,6 +1,4 @@
 import React from "react";
-import axios from "axios";
-import Button from '@material-ui/core/Button';
 import SavingsCard from './SavingsCard'
 import LeadForm from './LeadForm';
 import evenClient from './evenClient'
