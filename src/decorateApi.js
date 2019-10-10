df0140442af312b483d24b15867c124bec1c1283
Dan Siwiec <daniel.siwiec@gmail.com>
2019-10-09 22:04:41 -0700

    Add more offers

--- src/decorateApi.js
+++ src/decorateApi.js
@@ -1,5 +1,24 @@
+const bofa = template => {
+  let copy = JSON.parse(JSON.stringify(template))
+  copy.partner.imageUrl = 'https://media.bizj.us/view/img/11138963/new-bank-of-america-logo*750xx3000-1688-0-356.jpg'
+  return copy
+}
+
+const chase = template => {
+  let copy = JSON.parse(JSON.stringify(template))
+  copy.partner.imageUrl = 'http://graphicsmagazine.com/wp-content/uploads/2012/06/chaselogo2015-300x204.jpg'
+  return copy
+}
+
+const capitalOne = template => {
+  let copy = JSON.parse(JSON.stringify(template))
+  copy.partner.imageUrl = 'https://awardwallet.com/blog/wp-content/uploads/2017/04/Capital-One-Logo-Featured.jpg'
+  return copy
+}
+
 export default data => {
-  let savingsOffers = data.savingsOffers[0]
-  data.savingsOffers = [savingsOffers, savingsOffers, savingsOffers, savingsOffers]
+  let savingsOffer = data.savingsOffers[0]
+  data.savingsOffers = [savingsOffer, bofa(savingsOffer), chase(savingsOffer), capitalOne(savingsOffer)]
+  console.log(data)
   return data
 }
